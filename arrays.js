188a7489787f3dfc7cd90494efb9dc75721cf4a5
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-10 17:40:09 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,57 @@
+
+
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+];
+
+function addElementToBeginningOfArray(array, element) {
+    var newArray = [element, ...array];
+    return newArray;
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+
+  return array.unshift(element);
+/*  for (i = 1; i <= array.length - 1; i ++){
+    array[i] = array[i - 1];
+  }
+  array[0] = element;
+  return array; */
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array[array.length] = element;
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  var discarded = array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  var newArray = array.slice(0, array.length - 1);
+  return newArray;
+}
